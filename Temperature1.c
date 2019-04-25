dda261b96dc3b438cfc1941373f8f3e8cd076e87
nhce <rakshithsridharan@gmail.com>
2019-04-25 13:20:18 +0530

    Add files via upload

---
+++ Temperature1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/*program to convert farenheit to celsius*/
+int main()
+{
+	int fahr;
+	for(fahr=0;fahr<=300;fahr=fahr+20){
+		printf("%3d %6.2f\n",fahr,(5.0/9.0)*(fahr-32));
+	}
+	return 0;
+}
